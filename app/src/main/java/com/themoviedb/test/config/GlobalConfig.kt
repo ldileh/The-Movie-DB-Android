cffecf4f9cbeb7eb87312e1c1591dcd4626f6de4
ldileh <ldileh@gmail.com>
2024-02-05 12:52:29 +0700

    changelog: remove unused constant variable

--- app/src/main/java/com/themoviedb/test/config/GlobalConfig.kt
+++ app/src/main/java/com/themoviedb/test/config/GlobalConfig.kt
@@ -5,7 +5,4 @@ import com.themoviedb.test.BuildConfig
 object GlobalConfig {
     // check if build app is debug or not
     const val IS_DEBUG = BuildConfig.BUILD_TYPE == "debug"
-
-    // name of app
-    private const val APP_NAME = "app"
 }
