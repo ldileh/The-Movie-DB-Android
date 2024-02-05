1acbcc3de42624558aa17251a6fe177cf538f7e8
ldileh <ldileh@gmail.com>
2024-02-05 12:47:20 +0700

    changelog: create movie repository

---
+++ app/src/main/java/com/themoviedb/test/source/repository/MovieRepository.kt
@@ -0,0 +1,18 @@
+package com.themoviedb.test.source.repository
+
+import com.themoviedb.core.utils.Resource
+import com.themoviedb.test.model.source.remote.MovieDetailResponseModel
+import com.themoviedb.test.model.source.remote.MovieGenresResponseModel
+import com.themoviedb.test.model.source.remote.MovieResponseModel
+import com.themoviedb.test.model.source.remote.MovieReviewsResponseModel
+import com.themoviedb.test.model.source.remote.MovieVideosResponseModel
+import retrofit2.Response
+
+interface MovieRepository {
+
+    suspend fun getMovieGenres(): Resource<MovieGenresResponseModel>
+    suspend fun getMovies(page: Int, genres: List<Int>?): Response<MovieResponseModel>
+    suspend fun getDetailMovie(movieId: Int): Resource<MovieDetailResponseModel>
+    suspend fun getMovieReviews(page: Int, movieId: Int): Response<MovieReviewsResponseModel>
+    suspend fun getMovieVideos(movieId: Int): Resource<MovieVideosResponseModel>
+}
---
+++ app/src/main/java/com/themoviedb/test/source/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package com.themoviedb.test.source.repository
+
+import com.themoviedb.core.utils.Resource
+import com.themoviedb.test.model.source.remote.MovieDetailResponseModel
+import com.themoviedb.test.model.source.remote.MovieGenresResponseModel
+import com.themoviedb.test.model.source.remote.MovieResponseModel
+import com.themoviedb.test.model.source.remote.MovieReviewsResponseModel
+import com.themoviedb.test.model.source.remote.MovieVideosResponseModel
+import com.themoviedb.test.source.remote.MovieClient
+import retrofit2.Response
+import javax.inject.Inject
+
+class MovieRepositoryImpl @Inject constructor(
+    private val movieClient: MovieClient
+): MovieRepository {
+
+    override suspend fun getMovieGenres(): Resource<MovieGenresResponseModel> {
+        return movieClient.getMovieGenres()
+    }
+
+    override suspend fun getMovies(
+        page: Int,
+        genres: List<Int>?,
+    ): Response<MovieResponseModel> {
+        return movieClient.discoverMovies(page, genres)
+    }
+
+    override suspend fun getDetailMovie(movieId: Int): Resource<MovieDetailResponseModel> {
+        return movieClient.detailMovie(movieId)
+    }
+
+    override suspend fun getMovieReviews(
+        page: Int,
+        movieId: Int
+    ): Response<MovieReviewsResponseModel> {
+        return movieClient.movieReviews(page, movieId)
+    }
+
+    override suspend fun getMovieVideos(movieId: Int): Resource<MovieVideosResponseModel> {
+        return movieClient.movieVideos(movieId)
+    }
+}
