1f2d193a20108655b99e569d7df99be03eb4e950
ldileh <ldileh@gmail.com>
2024-02-06 08:36:01 +0700

    changelog: change error network message

--- core/src/main/java/com/themoviedb/core/exception/NoConnectivityException.kt
+++ core/src/main/java/com/themoviedb/core/exception/NoConnectivityException.kt
@@ -5,6 +5,6 @@ import java.io.IOException
 
 class NoConnectivityException : IOException() {
     override val message: String
-        get() = "No Internet Connection"
+        get() = "Sorry we can't connect"
     // You can send any message whatever you want from here.
 }
